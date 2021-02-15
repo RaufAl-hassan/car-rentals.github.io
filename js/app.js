d7a304b940b7ac09513d06f3a84fbcff69211573
Rauf Alhassan <alhassanr017@gmail.com>
2021-02-15 05:44:27 +0000

    init commit already created navbar, showacse and show top brands

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// UI VARIABLES
+const $header = document.querySelector("header"),
+  $nav = document.querySelector("nav"),
+  $hamburgerIcon = document.querySelector(".hamburger-icon"),
+  $navLinks = document.querySelector("nav .right-links");
+
+const hideNavLinks = () => {
+  $navLinks.classList.remove("show-nav-links");
+  $hamburgerIcon.classList.remove("fa-times");
+};
+
+// TOGGLE NAV LINKS (SHOW/HIDE)
+$hamburgerIcon.addEventListener("click", (e) => {
+  $navLinks.classList.toggle("show-nav-links");
+  e.target.classList.toggle("fa-times");
+});
+
+// CLOSE NAV LINKS WHEN MOUSE LEAVES NAV
+$nav.addEventListener("mouseleave", hideNavLinks);
+$nav.addEventListener("touchend", hideNavLinks);
+
+const options = {
+  rootMargin: "-100px 0px 0px 0px",
+};
+const navObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      $nav.classList.add("nav-change");
+    } else {
+      $nav.classList.remove("nav-change");
+    }
+  });
+}, options);
+navObserver.observe($header);
